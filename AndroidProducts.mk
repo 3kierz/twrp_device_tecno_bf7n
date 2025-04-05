c5078531f016f0c6e6f40f3009f14e72b70dd5ac
Ethan Kier <3kierz@gmail.com>
2025-04-05 02:44:21 -0400

    Create AndroidProducts.mk

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_BF7n.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_BF7n-user \
+    twrp_BF7n-userdebug \
+    twrp_BF7n-eng
